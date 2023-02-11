e7162f87e9ab1102d3d5bc46cce8863f552c480b
matthewtognotti <matthewtognotti@gmail.com>
2023-02-11 13:32:38 -0800

    added while loop to ensure connection is made between publisher and subscriber

--- ridgeback_script.py
+++ ridgeback_script.py
@@ -1,4 +1,4 @@
-# /usr/bin/env python
+#!/usr/bin/env python
 
 # Author: Matt Tognotti
 # Date: 9 Feburary 2023
@@ -11,15 +11,19 @@ import rospy
 from geometry_msgs.msg import Twist
 
 
-def move_forward():
-
-    print("\nStarting move_forward...")
+def move_ridgeback():
 
     # Name of node is move_forward, anonmyous = True ensures the name is unique by adding extra numberrs
     rospy.init_node('move_forward', anonymous="True")
 
-    # Publish to the ridgeback cmd_vel topic with type geometry_msgs/Twist
+    # Create publisher to the  cmd_vel topic with type geometry_msgs/Twist
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    # Note: when publishing to a topic once, sometimes the message is not receieved by the subscriber if
+    # there is not enough time between sending the message and the subscriber registering with the new publisher
+    # Therefore, we need to wait until the connection between this publisher and the subsriber is made
+    while pub.get_num_connections() < 1:
+        pass
 
     # Make sure ros is up and running
     if not rospy.is_shutdown():
@@ -32,15 +36,17 @@ def move_forward():
         twist.angular.y = 0.5
         twist.angular.z = 0.5
 
+        print("\nStarting move_forward...")
+
         # Print on screen and write to rosout
         rospy.loginfo(twist)
         # Publish to the topic cmd_vel
         pub.publish(twist)
-        print("Done\n")
+        print("Done!\n")
 
 
 if __name__ == "__main__":
     try:
-        move_forward()
+        move_ridgeback()
     except rospy.ROSInterruptException:
         pass
